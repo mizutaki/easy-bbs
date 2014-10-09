7a2981a37a653642895131ef86a08b994e5b0bc9
mizutaki <mizutaki4@gmail.com>
2014-10-09 21:50:56 +0900

    pulled out a common part.

--- lib/bbs_server.rb
+++ lib/bbs_server.rb
@@ -3,16 +3,16 @@ require_relative 'bean/contribution_info'
 store = []
 
 get '/'  do
-	erb :index
+	erb :index, :layout => false
 end
 
 get '/main' do
-	erb :main
+	erb :main, :layout => :form
 end
 
 post '/write' do
 	content = ContributionInfo.new(params[:name], params[:message],Time.now.strftime('%Y/%m/%d %H:%M:%S'))
 	store << content
 	@store = store.clone
-	erb :write
+	erb :write, :layout => :form
 end
