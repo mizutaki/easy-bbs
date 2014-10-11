3acb9e3df635918e03417c1236df56caf6101d90
mizutaki <mizutaki4@gmail.com>
2014-10-11 23:20:06 +0900

    one page ten items show.

--- lib/bbs_server.rb
+++ lib/bbs_server.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require_relative 'bean/contribution_info'
 store = []
+current_page = 1
+start = 0
 
 get '/'  do
 	erb :index, :layout => false
@@ -12,7 +14,31 @@ end
 
 post '/write' do
 	content = ContributionInfo.new(params[:name], params[:message],Time.now.strftime('%Y/%m/%d %H:%M:%S'))
-	store << content
-	@store = store.clone
+	store.unshift(content)
+	@store = store[0, 10]
+	@current_page = current_page
 	erb :write, :layout => :form
 end
+
+get '/next/:page' do
+	en = 10
+	start = 10 * current_page + 1
+	current_page += 1
+	@store =  store[start, en]
+	@current_page = current_page
+	erb :write, :layout => :form
+end
+
+get '/prev' do
+	en = 10
+	current_page -= 1
+	if current_page == 1
+		start = 0
+	else
+		start -=10
+	end
+	@store = store[start,en]
+	@current_page = current_page
+	erb :write, :layout => :form
+end
+
